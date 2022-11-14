52af11523fe7251666ae6fe8534ae73cbd9f0be9
steven4program <stevenchang421@gmail.com>
2022-11-14 21:59:51 +0800

    add handlebars

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const { engine } = require('express-handlebars')
+
+const port = process.env.PORT || 3000
 
 mongoose.connect('mongodb://localhost/todo_list')
 
@@ -14,10 +17,28 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
+app.engine('.hbs', engine({ extname: '.hbs', defaultLayout: 'main' }))
+app.set('view engine', '.hbs')
+
 app.get('/', (req, res) => {
-  res.send('This is a todo list app.')
+  res.render('index')
+})
+
+app.use((req, res) => {
+  res.type('text/plain')
+  res.status(404)
+  res.send('404 - Not Found')
+})
+
+app.use((err, req, res, next) => {
+  res.type('text/plain')
+  res.status(500)
+  res.send('500 - Server Error')
 })
 
 app.listen(3000, () => {
-  console.log('App is running on http://localhost/3000')
+  console.log(
+    `App is running on http://localhost/${port}; ` +
+      'press Ctrl-C to terminate.'
+  )
 })
