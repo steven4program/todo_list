19a88f89d26407f7e2f318fee60226281b34789f
steven4program <stevenchang421@gmail.com>
2022-12-10 20:38:44 +0800

    sort data before rendering

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ app.use(express.urlencoded({ extended: true }))
 app.get('/', (req, res) => {
   Todo.find()
     .lean()
+    .sort({ _id: 'asc' })
     .then((todos) => res.render('index', { todos }))
     .catch((err) => console.error(err))
 })
