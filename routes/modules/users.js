5e4c9444423e45181c096f6364107a7314dcc6ae
steven4program <stevenchang421@gmail.com>
2022-12-18 19:42:34 +0800

    add bcrypt.js

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const passport = require('passport')
+const bcrypt = require('bcryptjs')
 const User = require('../../models/user')
 
 router.get('/login', (req, res) => {
@@ -49,7 +50,6 @@ router.post('/register', (req, res) => {
       confirmPassword
     })
   }
-
   User.findOne({ email }).then((user) => {
     if (user) {
       errors.push({ message: '這個 Email 已經註冊過了。' })
@@ -61,11 +61,16 @@ router.post('/register', (req, res) => {
         confirmPassword
       })
     }
-    return User.create({
-      name,
-      email,
-      password
-    })
+    return bcrypt
+      .genSalt(10)
+      .then((salt) => bcrypt.hash(password, salt))
+      .then((hash) =>
+        User.create({
+          name,
+          email,
+          password: hash
+        })
+      )
       .then(() => res.redirect('/'))
       .catch((err) => console.log(err))
   })
