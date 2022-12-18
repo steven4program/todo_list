a292f2926fe371a3e8dff782384cc44f35a229ad
steven4program <stevenchang421@gmail.com>
2022-12-18 19:51:14 +0800

    add bcrypt to login

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,6 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
+const bcrypt = require('bcryptjs')
 const User = require('../models/user')
 
 module.exports = (app) => {
@@ -18,13 +19,16 @@ module.exports = (app) => {
                 false,
                 req.flash('warning_msg', 'That email is not registered!')
               )
-            if (user.password !== password)
-              return done(
-                null,
-                false,
-                req.flash('warning_msg', 'Email or Password incorrect.')
-              )
-            return done(null, user)
+            return bcrypt.compare(password, user.password).then((isMatch) => {
+              if (!isMatch) {
+                return done(
+                  null,
+                  false,
+                  req.flash('warning_msg', 'Email or Password incorrect.')
+                )
+              }
+              return done(null, user)
+            })
           })
           .catch((err) => done(err, false))
       }
